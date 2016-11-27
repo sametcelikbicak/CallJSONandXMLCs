48b1474bac678b76299a49fa2eb7929fddb148cb
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2016-11-27 16:47:07 +0300

    First commit.

---
+++ CallJSONandXMLCs/CallJSONandXMLCs/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace CallJSONandXMLCs
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ CallJSONandXMLCs/CallJSONandXMLCs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace CallJSONandXMLCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JSONandXMLCs.JSONandXMLSoapClient xmlClient = new JSONandXMLCs.JSONandXMLSoapClient();
+            var teamFromXML = xmlClient.GetTeamFromXML();
+            Console.WriteLine("XML'den gelen sonuçlar:");
+            foreach (var item in teamFromXML)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Position + " " + item.Salary);
+            }
+            Console.WriteLine();
+
+            JSONandXMLCs.JSONandXMLSoapClient jsonClient = new JSONandXMLCs.JSONandXMLSoapClient();
+            var teamFromJSON = jsonClient.GetTeamFromJSON();
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            List<Member> members = new List<Member>();
+            members = jss.Deserialize<List<Member>>(teamFromJSON);
+            Console.WriteLine("JSON'dan gelen sonuçlar:");
+            foreach (var item in members)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Position + " " + item.Salary);
+            }
+            Console.ReadKey();
+        }
+    }
+}
